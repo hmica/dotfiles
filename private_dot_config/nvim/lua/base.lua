7e14a82fb20518cf897a6fec14e833ff8e3bfbcd
Mica Hallin <mica@hallin.be>
2023-01-10 15:56:39 +0100

    add nvim config

---
+++ private_dot_config/nvim/lua/base.lua
@@ -0,0 +1,27 @@
+vim.cmd('autocmd!')
+
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+
+vim.wo.number = true
+vim.opt.autoindent = true
+vim.opt.hlsearch = true
+vim.opt.backup = false
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.laststatus = 2
+vim.opt.expandtab = true
+vim.opt.scrolloff = 10
+vim.opt.shell = 'fish'
+vim.opt.backupskip = '/tmp/*,/private/tmp/*'
+vim.opt.inccommand = 'split'
+vim.opt.ignorecase = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.ai = true -- auto Indent
+vim.opt.wrap = false -- no wrap lines
+vim.opt.backspace = 'start,eol,indent'
+vim.opt.path:append { '**' } -- finding files - search into subfolders
+vim.opt.wildignore:append { '*/node_modules/*' }
+
+
